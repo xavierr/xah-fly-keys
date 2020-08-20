c1b120c3ee613455fd4c90b4aeeeca34db744a0c
Xavier Raynaud <xav.raynaud@gmail.com>
2020-08-20 15:02:52 +0200

    added annot shortcut

--- extra-mode-bindings.el
+++ extra-mode-bindings.el
@@ -302,6 +302,7 @@ by my- which switches to insert mode after execution"
   (define-key pdf-view-mode-map "a" 'pdf-annot-add-highlight-markup-annotation)
   (define-key pdf-view-mode-map "\C-f" 'pdf-links-isearch-link)
   (define-key pdf-view-mode-map "3" 'delete-other-windows)
+  (define-key pdf-view-mode-map "1" 'pdf-annot-add-highlight-markup-annotation)
   (require 'pdf-links)
   (define-key pdf-links-minor-mode-map "f" nil)
   (define-key pdf-view-mode-map "f" 'isearch-forward)
