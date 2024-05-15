6f5470330fee0e5687e2262a7b46e5e70aa82670
Xavier Raynaud <xav.raynaud@gmail.com>
2024-05-15 17:04:23 +0200

    added LaTeX-mode keybinding (previous latex-mode name got changed)

--- extra-mode-bindings.el
+++ extra-mode-bindings.el
@@ -109,6 +109,7 @@
    ((eq major-mode 'python-mode) (define-key xah-fly-key-map (kbd "æ") 'elpy-command-map))
    ((eq major-mode 'dired-mode) (define-key xah-fly-key-map (kbd "æ") 'dired-command-map))
    ((eq major-mode 'latex-mode) (define-key xah-fly-key-map (kbd "æ") 'latex-extra-keymap))
+   ((eq major-mode 'LaTeX-mode) (define-key xah-fly-key-map (kbd "æ") 'latex-extra-keymap))
    ((eq major-mode 'matlab-shell-mode) (define-key xah-fly-key-map (kbd "æ") 'matlab-shell-debug-keymap))
    ((eq major-mode 'vterm-mode) (xavier/vterm-setup-xah-command-mode))
    (t nil)))
